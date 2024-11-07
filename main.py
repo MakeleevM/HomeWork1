206304020567ee4b2abad05c8cf56c1dbde18abd
MakeleevM <88980894+MakeleevM@users.noreply.github.com>
2024-11-07 09:26:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3rd program
+a=2*2+2
+b=2*(2+2)
+print(a)
+print(b)
+print(a==b)
+
+# 4th program
+a='123.456'
+num=float(a)
+num=num*10
+int_num=int(num)
+result=int_num%10
+print(result)
